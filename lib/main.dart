934edcb09aaadb86883213c02efaacc14bbdc45d
Muathye <muath.ye@gmail.com>
2020-11-17 07:56:40 +0300

    solve step 24

--- lib/main.dart
+++ lib/main.dart
@@ -103,6 +103,7 @@ class _StoryPageState extends State<StoryPage> {
   }
 }
 
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+//TODO: Done Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+// Step 24 solution: add setState()
 
 //TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
