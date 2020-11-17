73fbb6069008999de30c29eda6215d452fae6a68
Muathye <muath.ye@gmail.com>
2020-11-17 08:26:17 +0300

    solve step 27

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -99,6 +99,11 @@ class StoryBrain {
   restart() {
     _storyNumber = 0;
   }
+
+  //TODO: Done Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  buttonShouldBeVisible() {
+    return _storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2;
+  }
 }
 
 
@@ -106,4 +111,3 @@ class StoryBrain {
 
 //TODO: Done Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
